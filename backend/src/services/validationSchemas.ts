b7fcbae3c6537c0691f950afa3ed713e9c4dc17d
Carlota de la Vega <carlotadlavega@gmail.com>
2024-10-14 17:59:08 +0200

    Definición de esquemas de validación. Closes #23

---
+++ backend/src/services/validationSchemas.ts
@@ -0,0 +1,23 @@
+import Joi from 'joi';
+
+
+export const userSchema = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+    role: Joi.string().valid('admin', 'user', 'guest').required(),
+});
+
+export const cardSchema = Joi.object({
+    name: Joi.string().required(),
+    rarity: Joi.string().required(),
+    image: Joi.string().uri().required(),
+    description: Joi.string().required(),
+    userId: Joi.number().required(),
+});
+
+export const tradeSchema = Joi.object({
+    offeredCardId: Joi.number().required(),
+    requestedCardId: Joi.number().required(),
+    offeredUserId: Joi.number().required(),
+    requestedUserId: Joi.number().required(),
+});
