54f90399ffd97c54fc23e6667e0d54336bf3babe
Carlota de la Vega <carlotadlavega@gmail.com>
2024-10-14 17:48:06 +0200

    Creado modelo de usuario. Closes #10

---
+++ backend/src/models/User.ts
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../config/database";
+import Trade from "./Trade";
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public password!: string;
+  public role!: string;
+}
+
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+  }
+);
+
+export default User;
