d9db4a79eb05161d8251db5beb37b594f23a12ec
Carlota de la Vega <carlotadlavega@gmail.com>
2024-11-01 19:12:46 +0100

    Main app. Closes #25

---
+++ backend/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+import userRoutes from "./routes/userRoutes";
+import cardRoutes from "./routes/cardRoutes";
+import tradeRoutes from "./routes/tradeRoutes";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { swaggerOptions } from "./services/swagger";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use("/api/users", userRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("/api/trades", tradeRoutes);
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Ruta raíz
+app.get("/", (req, res) => {
+  res.redirect("/docs");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
