0a4095b569847cc590d3c0b1e42b4722a3a0a918
Carlota de la Vega <carlotadlavega@gmail.com>
2024-11-24 19:27:15 +0100

    Prueba de workflow 5

--- backend/src/config/sync_database.ts
+++ backend/src/config/sync_database.ts
@@ -5,7 +5,7 @@ import "../models/index";
 
 dotenv.config();
 
-const createDatabaseIfNotExists = async () => {
+const dropDatabaseIfExists = async () => {
   const dbName = process.env.TEST_DB_DATABASE || "pokemon_trading_test";
   const dbUsername = process.env.TEST_DB_USERNAME || "pokemon_admin";
   const dbPassword = process.env.TEST_DB_PASSWORD || "Pokemon";
@@ -17,6 +17,31 @@ const createDatabaseIfNotExists = async () => {
   });
 
   try {
+    console.log(
+      `Intentando eliminar la base de datos "${dbName}" si existe...`
+    );
+    await adminSequelize.query(`DROP DATABASE IF EXISTS "${dbName}"`);
+    console.log(`Base de datos "${dbName}" eliminada.`);
+  } catch (error: any) {
+    console.error("Error al eliminar la base de datos:", error.message);
+  } finally {
+    await adminSequelize.close();
+  }
+};
+
+const createDatabase = async () => {
+  const dbName = process.env.TEST_DB_DATABASE || "pokemon_trading_test";
+  const dbUsername = process.env.TEST_DB_USERNAME || "pokemon_admin";
+  const dbPassword = process.env.TEST_DB_PASSWORD || "Pokemon";
+  const dbHost = process.env.TEST_DB_HOST || "127.0.0.1";
+
+  const adminSequelize = new Sequelize("postgres", dbUsername, dbPassword, {
+    host: dbHost,
+    dialect: "postgres",
+  });
+
+  try {
+    console.log(`Creando la base de datos "${dbName}"...`);
     await adminSequelize.query(`CREATE DATABASE "${dbName}"`);
     console.log(`Base de datos "${dbName}" creada exitosamente.`);
   } catch (error: any) {
@@ -32,7 +57,8 @@ const createDatabaseIfNotExists = async () => {
 
 const syncDatabase = async () => {
   try {
-    await createDatabaseIfNotExists();
+    await dropDatabaseIfExists();
+    await createDatabase();
     await sequelize.sync({ force: true });
     console.log("Modelos sincronizados con la base de datos.");
   } catch (error) {
